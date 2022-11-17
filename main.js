1f2df9058571b96ee4a1709663804635444162d8
rufatbayramov90 <rufatbyrmv@gmail.com>
2022-11-17 18:50:44 +0400

    Javascript Change

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+let cartIcon = document.querySelector('#cart-icon');
+let cart = document.querySelector('.cart');
+let closeCart = document.querySelector('#close-cart');
+// Open cart
+cartIcon.onclick = () =>{
+    cart.classList.add('active'); 
+}
+// Close Cart
+cartClose.onclick = () =>{
+    cart.classList.remove('active'); 
+}
 
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready);
+}else{
+    ready();
+}
+
+//Making Function
+function ready(){
+    //Remove Items From Cart
+    var reomveCartButtons = document.getElementsByClassName('cart-remove')
+}
